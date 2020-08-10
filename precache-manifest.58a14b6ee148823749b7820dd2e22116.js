6af7194718ecf34834afed224063bb67d85c67ae
anshu-20166 <anshu.kumari20166@gmail.com>
2020-08-10 21:11:10 +0530

    Updates

---
+++ precache-manifest.58a14b6ee148823749b7820dd2e22116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc0e523c29c57bbf72836541e91a4fe",
+    "url": "/Cart/index.html"
+  },
+  {
+    "revision": "15837c587cb907fceaf8",
+    "url": "/Cart/static/css/main.7842fb33.chunk.css"
+  },
+  {
+    "revision": "e031b0b7fe37195565be",
+    "url": "/Cart/static/js/2.c88c22ab.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/Cart/static/js/2.c88c22ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15837c587cb907fceaf8",
+    "url": "/Cart/static/js/main.b6d9082a.chunk.js"
+  },
+  {
+    "revision": "a993e9164943af8f9819",
+    "url": "/Cart/static/js/runtime-main.41d58a9e.js"
+  }
+]);
